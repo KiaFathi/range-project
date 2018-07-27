c3ddfee6db9a9390792da3471979b86d642b2bb9
Kia Fathi <kiafathi@me.com>
2018-07-27 14:09:36 -0700

    (feat) User add onClick prop and circular img

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 
-export default props => (
-  <Card>
+const User = props => (
+  <Card onClick={props.onClick}>
     <Card.Content>
-      <Image floated="right" size="mini" src={props.avatar} />
+      <Image floated="right" size="mini" circular src={props.avatar} />
       <Card.Header>{props.name}</Card.Header>
     </Card.Content>
   </Card>
 );
+
+export default User;
